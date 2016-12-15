6b2139a5d1b112c0b6f9505549e109d827d5e438
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-12-15 12:18:19 +0900

    Fix svg tile generator

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from 'express'
 import { PNG } from 'pngjs'
 import { tile2lat, tile2lon } from './tileConvert'
-import * as d3 from 'd3'
-import { jsdom } from 'jsdom'
-
 
 // sample 100 points
 const hotSpotCenters = Array.from(Array(100).keys()).map(() => {
@@ -32,15 +29,47 @@ const isHotSpot = (spot) => {
 
 const createSVGText = opt => {
 
-  const colors = ['AliceBlue', 'HoneyDew', 'MistyRose', 'Tan']
-  const getColor = () => colors[Math.floor(Math.random() * colors.length)]
+  const colors = [
+    "#1ca69e",
+    "#188fc2",
+    "#bb625e",
+    "#09740a",
+  ]
+  const colorChars = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f']
+  const getColorChar = () => colorChars[Math.floor(Math.random() * colorChars.length)]
+  const getColor = () => '#' + getColorChar() + getColorChar() + getColorChar()
+  const getLength = () => Math.floor(Math.random() * 100)
 
   return `
-  <svg width="256" height="256" viewBox="0 0 256 256"  xmlns="http://www.w3.org/2000/svg">
-    <circle cx="64" cy="64" r="50" fill="${getColor()}" />
-    <circle cx="64" cy="192" r="50" fill="${getColor()}" />
-    <circle cx="192" cy="64" r="50" fill="${getColor()}" />
-    <circle cx="192" cy="192" r="50" fill="${getColor()}" />
+  <svg height="100" width="100" viewbox="0 0 256 256" xmlns="http://www.w3.org/2000/svg">
+    <rect x="10" y="50" width="100" height="75" stroke="red" fill="blue" stroke-width="5" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <circle cx="${getLength()}" cy="${getLength()}" r="${getLength() / 2}" fill="${getColor()}" />
+    <text x="10" y="8" font-family="Verdana" font-size="8" fill="#555">z = ${opt.z}</text>
+    <text x="10" y="16" font-family="Verdana" font-size="8" fill="#555">x = ${opt.x}</text>
+    <text x="10" y="24" font-family="Verdana" font-size="8" fill="#555">y = ${opt.y}</text>
   </svg>
   `
 }
@@ -158,7 +187,8 @@ express()
 
     res.write(createSVGText({
       width: 256,
-      height: 256
+      height: 256,
+      z, x, y
     }))
     res.end()
 
